6eeb566e868d3eee5d721a07b5e19a516bc03776
Matias Isella <matias.isella@glovoapp.com>
2022-07-13 14:37:42 +0200

    Other commit

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { promises: fs } = require("fs");
 
-try {
-  console.log(`Le run succed!`);
-} catch (error) {
-  core.setFailed(error.message);
+async function getFiles(path = "./") {
+  const entries = await fs.readdir(path, { withFileTypes: true });
+
+  // Get files within the current directory and add a path key to the file objects
+  const files = entries
+    .filter(file => !file.isDirectory()) // Skip directories
+    .map(file => ({ ...file, path: path + file.name }));
+
+  // Get folders within the current directory
+  const folders = entries.filter(folder => folder.isDirectory());
+
+  for (const folder of folders)
+    /*
+      Add the found files within the subdirectory to the files array by calling the
+      current function itself
+    */
+    files.push(...await getFiles(`${path}${folder.name}/`));
+
+  return files;
 }
+
+// try {
+
+//   const files = await getFiles()
+
+//   console.log(files)
+//   //console.log(`Le run succed!`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
+
+// Call start
+(async () => {
+  console.log(await (await getFiles()))
+})();
